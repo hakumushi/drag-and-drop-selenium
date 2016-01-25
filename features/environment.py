765d7e7a75ca466b1042ae7c2e33715af7572234
prff <prff@cin.ufpe.br>
2016-01-25 18:58:38 -0300

    folder name correction

--- features/environment.py
+++ features/environment.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 import os
 
 def before_all(context):
-    context.driver = webdriver.Chrome(executable_path=os.path.dirname(os.path.realpath(__file__)) + "/resources/chromedriver")
-    #context.driver = webdriver.Firefox()
+    #context.driver = webdriver.Chrome(executable_path=os.path.dirname(os.path.realpath(__file__)) + "/resources/chromedriver")
+    context.driver = webdriver.Firefox()
     context.driver.implicitly_wait(30)
     context.driver.maximize_window()
 
